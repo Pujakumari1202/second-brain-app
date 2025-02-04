eb8ac7eb28b9e7a191a773a466630fd3799a5eed
Puja Kumari <149191257+Pujakumari1202@users.noreply.github.com>
2025-02-04 21:09:34 +0530

    update all files and folder and code of generic button components

---
+++ frontend/src/icons/index.ts
@@ -0,0 +1,11 @@
+// properties of all icon defined here sp un future any icon can be used this
+export interface IconProps{
+    size:"sm" | "md" | "lg";
+
+}
+
+export const iconSizeVariants={
+    "sm":"size-2",
+    "md":"size-4",  
+    "lg":"size-6"
+}
