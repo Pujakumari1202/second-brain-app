e7f2f31daf801c07c78327ec18f347904a5ba79d
Puja Kumari <149191257+Pujakumari1202@users.noreply.github.com>
2025-02-06 23:28:31 +0530

    commit all the folders and files

--- frontend/src/components/Sidebar.tsx
+++ frontend/src/components/Sidebar.tsx
@@ -4,7 +4,7 @@ import { YoutubeIcon } from "../icons/YoutubeIcon";
 import { SidebarItem } from "./SidebarItem";
 
 export function Sidebar(){
-    return <div className="h-screen bg-white  w-72 fixed left-0 top-0 pl-6 border border-gray-300">
+    return <div className="h-screen bg-white  w-72 fixed left-0 top-0 pl-6 border border-gray-200">
         <div className="flex text-2xl pt-8 items-center ">
             <div className="pr-2 text-purple-600">
                <Logo/>
--- frontend/src/pages/dashboard.tsx
+++ frontend/src/pages/dashboard.tsx
@@ -1,37 +1,52 @@
-import {Button } from '../components/Button';
-import {PlusIcon} from "../icons/PlusIcon";
-import {ShareIcon} from "../icons/ShareIcon";
-import {Card} from "../components/Card";
-import { CreateContentModel } from '../components/CreateContentModel';
-import { useState } from 'react';
-import { Sidebar } from '../components/Sidebar';
+import { useEffect, useState } from "react"
+import { Button } from "../components/Button"
+import { Card } from "../components/Card"
+import { CreateContentModal } from "../components/CreateContentModal"
+import { PlusIcon } from "../icons/PlusIcon"
+import { ShareIcon } from "../icons/ShareIcon"
+import { Sidebar } from "../components/Sidebar"
+import { useContent } from "../hooks/useContent"
+import { BACKEND_URL } from "../config"
+import axios from "axios"
 
 export function Dashboard() {
-  const [modalOpen, setModalOpen] = useState(true);
+  const [modalOpen, setModalOpen] = useState(false);
+  const {contents, refresh} = useContent();
 
-  return  <div>
-    <Sidebar/>
-      <div className="p-4 ml-72 min-h-screen bg-gray-100 ">
-        <CreateContentModel open={modalOpen} onClose={()=>setModalOpen(false)}/>
-        <div className="flex justify-end gap-4">
-        <Button onClick={()=>{
-          setModalOpen(true);
-        }} variant ="primary" text="Add Content" startIcon={<PlusIcon/>}>
-        </Button>
-        <Button variant ="secondary" text="Share brain" startIcon={<ShareIcon/>}>
-        </Button>
+  useEffect(() => {
+    refresh();
+  }, [modalOpen])
 
-        </div>
-
-        <div className="flex gap-4">
-        <Card type="twitter" link="https://x.com/kirat_tw/status/1633685473821425666" title="First tweet" />
-        <Card type="youtube" link="https://www.youtube.com/watch?v=Oo3qsxihXqY" title="First video" />
+  return <div>
+    <Sidebar />
+    <div className="p-4 ml-72 min-h-screen bg-gray-100 border-2">
+      <CreateContentModal open={modalOpen} onClose={() => {
+        setModalOpen(false);
+      }} />
+      <div className="flex justify-end gap-4">
+        <Button onClick={() => {
+          setModalOpen(true)
+        }} variant="primary" text="Add content" startIcon={<PlusIcon />}></Button>
+        <Button onClick={async () => {
+            const response = await axios.post(`${BACKEND_URL}/api/v1/brain/share`, {
+                share: true
+            }, {
+                headers: {
+                    "Authorization": localStorage.getItem("token")
+                }
+            });
+            const shareUrl = `http://localhost:5173/share/${response.data.hash}`;
+            alert(shareUrl);
+        }} variant="secondary" text="Share brain" startIcon={<ShareIcon />}></Button>
       </div>
-   </div>
-  
-   </div>
-    
-   
-  
-}
 
+      <div className="flex gap-4 flex-wrap">
+        {contents.map(({type, link, title}) => <Card 
+            type={type}
+            link={link}
+            title={title}
+        />)}
+      </div>
+    </div>
+  </div>
+}
